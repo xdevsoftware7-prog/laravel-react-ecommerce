0eedc033c544b635e6d4879cccc2cf31137913dc
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-17 11:13:31 +0100

    Déplacer les fichiers réutilisable dans le dossier Core et les plus spécifique dans le dossier App

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -1,7 +1,7 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import Dropdown from '@/Components/Dropdown';
-import NavLink from '@/Components/NavLink';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
+import ApplicationLogo from '@/Components/App/ApplicationLogo';
+import Dropdown from '@/Components/Core/Dropdown';
+import NavLink from '@/Components/Core/NavLink';
+import ResponsiveNavLink from '@/Components/Core/ResponsiveNavLink';
 import { Link, usePage } from '@inertiajs/react';
 import { PropsWithChildren, ReactNode, useState } from 'react';
 
