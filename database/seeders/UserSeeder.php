a7485f3be6704d0d6ba9db1cad80c828277bfd91
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-28 10:31:15 +0100

    Création de trois users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\RolesEnum;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::factory([
+            'name' => 'user',
+            'email' => 'user@example.com'
+        ])->afterCreating(function (User $user) {
+            $user->assignRole(RolesEnum::User->value);
+        })->create();
+
+        User::factory(['name' => "vendor", 'email' => "vendor@example.com"])->afterCreating(function (User $user) {
+            $user->assignRole(RolesEnum::Vendor->value);
+        })->create();
+        User::factory(['name' => 'admin', 'email' => 'admin@example.com'])->afterCreating(function (User $user) {
+            $user->assignRole(RolesEnum::Admin->value);
+        })->create();
+    }
+}
