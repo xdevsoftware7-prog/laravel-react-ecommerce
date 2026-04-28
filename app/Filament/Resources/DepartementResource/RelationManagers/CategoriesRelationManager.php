75202d6bfd5e9f45231403ea0f2edac562f163c4
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-28 16:34:57 +0100

    Definir le formulaire categorie departement

--- app/Filament/Resources/DepartementResource/RelationManagers/CategoriesRelationManager.php
+++ app/Filament/Resources/DepartementResource/RelationManagers/CategoriesRelationManager.php
@@ -2,10 +2,16 @@
 
 namespace App\Filament\Resources\DepartementResource\RelationManagers;
 
+use App\Models\Category;
 use Filament\Forms;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -16,11 +22,16 @@ class CategoriesRelationManager extends RelationManager
 
     public function form(Form $form): Form
     {
+        $departement = $this->getOwnerRecord();
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')
+                TextInput::make('name')
                     ->required()
                     ->maxLength(255),
+                Select::make('parent_id')->options(function () use ($departement) {
+                    return Category::query()->where('departement_id', $departement->id)->pluck('name', 'id')->toArray();
+                })->label('Parent Category')->preload()->searchable(),
+                Checkbox::make('active')
             ]);
     }
 
@@ -29,7 +40,10 @@ class CategoriesRelationManager extends RelationManager
         return $table
             ->recordTitleAttribute('name')
             ->columns([
-                Tables\Columns\TextColumn::make('name'),
+                TextColumn::make('name')->sortable()->searchable(),
+                TextColumn::make('parent.name')->sortable()->searchable(),
+                IconColumn::make('active')->boolean()
+
             ])
             ->filters([
                 //
