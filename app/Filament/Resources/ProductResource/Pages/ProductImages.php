efece0b2e30f241f9ef1d10367c569fd243c9ef9
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-29 06:52:45 +0100

    Ajout et configuration de spatie media library

---
+++ app/Filament/Resources/ProductResource/Pages/ProductImages.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Resources\ProductResource\Pages;
+
+use App\Filament\Resources\ProductResource;
+use Filament\Actions;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Form;
+use Filament\Resources\Pages\EditRecord;
+use Override;
+
+class ProductImages extends EditRecord
+{
+    protected static string $resource = ProductResource::class;
+
+    public function form(Form $form): Form
+    {
+        return $form->schema(
+            [
+                SpatieMediaLibraryFileUpload::make('images')->image()->multiple()->openable()->panelLayout("grid")->collection('images')->reorderable()->appendFiles()->preserveFilenames()->columnSpan(2)
+            ]
+        );
+    }
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
