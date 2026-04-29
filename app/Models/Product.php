c53b469042fe421a6f9a07af4f5e4af8f4993fa2
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-29 16:06:30 +0100

    Add Variation type methode inside the product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -27,4 +27,9 @@ class Product extends Model implements HasMedia
     {
         return $this->belongsTo(Category::class, 'category_id');
     }
+
+    public function variationTypes()
+    {
+        return $this->hasMany(VariationType::class);
+    }
 }
