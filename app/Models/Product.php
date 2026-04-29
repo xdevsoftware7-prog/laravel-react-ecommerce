bd71bc52c6693f0682cd08903ef47736d10de608
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-29 06:36:33 +0100

    Ajout de ProductImage et et le registerMediaCoversions

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,10 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 use Override;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class Product extends Model implements HasMedia
 {
     use InteractsWithMedia;
+
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')->width(100);
+        $this->addMediaConversion('small')->width(480);
+        $this->addMediaConversion('large')->width(1200);
+    }
 
     public function departement()
     {
