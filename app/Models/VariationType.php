82fc28083a4e856327d49dfb6a86a13fa99451aa
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-29 16:08:28 +0100

    Ajout d'une methode options dans le model VariationType

---
+++ app/Models/VariationType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VariationType extends Model
+{
+    public function options()
+    {
+        return $this->hasMany(VariationTypeOption::class, 'variation_type_id');
+    }
+}
