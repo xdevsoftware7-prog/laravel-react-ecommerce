c14e33d46a1cbcd1f1d23102ddcaf827f6745bdf
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-04-29 06:17:54 +0100

    Ajout de l'implementation de 'HasMedia' et le trait 'InteractWithMedia'

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Override;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Product extends Model
+class Product extends Model implements HasMedia
 {
+    use InteractsWithMedia;
+
     public function departement()
     {
         return $this->belongsTo(Departement::class, 'departement_id');
