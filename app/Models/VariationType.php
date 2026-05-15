458ed307801c16d7bd5f052edd0fad2028754761
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-05-15 19:39:42 +0100

    disable the timesstamps

--- app/Models/VariationType.php
+++ app/Models/VariationType.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class VariationType extends Model
 {
+    public $timestamps = false;
     public function options()
     {
         return $this->hasMany(VariationTypeOption::class, 'variation_type_id');
