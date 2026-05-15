3425e3df946573cfc4973c3063364c1de2cd2331
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-05-15 19:37:22 +0100

    implements the interface HasMedia and add the function registerMediaConversions

---
+++ app/Models/VariationTypeOption.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class VariationTypeOption extends Model implements HasMedia
+{
+    use InteractsWithMedia;
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')->width(100);
+        $this->addMediaConversion('small')->width(480);
+        $this->addMediaConversion('large')->width(1200);
+    }
+}
