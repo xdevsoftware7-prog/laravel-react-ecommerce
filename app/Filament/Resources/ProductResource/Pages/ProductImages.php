f94a0636eacb398a5ab448c537967ac5d312fd4d
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-05-15 19:54:47 +0100

    Add the title for the productimages and disbled the label

--- app/Filament/Resources/ProductResource/Pages/ProductImages.php
+++ app/Filament/Resources/ProductResource/Pages/ProductImages.php
@@ -12,13 +12,14 @@ use Override;
 class ProductImages extends EditRecord
 {
     protected static string $resource = ProductResource::class;
+    protected static ?string $title = 'Images';
     protected static ?string $navigationIcon = 'heroicon-s-photo';
 
     public function form(Form $form): Form
     {
         return $form->schema(
             [
-                SpatieMediaLibraryFileUpload::make('images')->image()->multiple()->openable()->panelLayout("grid")->collection('images')->reorderable()->appendFiles()->preserveFilenames()->columnSpan(2)
+                SpatieMediaLibraryFileUpload::make('images')->label(false)->image()->multiple()->openable()->panelLayout("grid")->collection('images')->reorderable()->appendFiles()->preserveFilenames()->columnSpan(2)
             ]
         );
     }
