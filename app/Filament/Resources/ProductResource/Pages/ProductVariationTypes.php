6060d9cc1ee7735afdd6f7e0bc441ac611908b02
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-05-15 19:27:44 +0100

    Create the ProductVariationTypes

---
+++ app/Filament/Resources/ProductResource/Pages/ProductVariationTypes.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filament\Resources\ProductResource\Pages;
+
+use App\Enums\Enums\ProductVariationTypesEnum;
+use App\Filament\Resources\ProductResource;
+use Filament\Actions;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Pages\EditRecord;
+use Override;
+
+class ProductVariationTypes extends EditRecord
+{
+    protected static string $resource = ProductResource::class;
+    protected static ?string $navigationIcon = 'heroicon-s-photo';
+
+    public function form(Form $form): Form
+    {
+        return $form->schema(
+            [
+                Repeater::make('variationTypes')->relationship()->collapsible()->defaultItems(1)->addActionLabel('Add new variation type')->columns(2)->columnSpan(2)->schema([
+                    TextInput::make('name')->required(),
+                    Select::make('type')->options(ProductVariationTypesEnum::lables())->required(),
+                    Repeater::make('options')->relationship()->collapsible()->schema([
+                        TextInput::make('name')->columnSpan(2)->required(),
+                        SpatieMediaLibraryFileUpload::make('images')->image()->multiple()->openable()->panelLayout('grid')->collection('images')->reorderable()->appendFiles()->preserveFilenames()->columnSpan(2)
+                    ])->columnSpan(2)
+                ])
+            ]
+        );
+    }
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
