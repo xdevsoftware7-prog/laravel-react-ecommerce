37b0cd8ae12410cbda226a7108cb4d08ab2fb4ec
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-05-15 19:53:02 +0100

    Add the title for the productvariationtypes and disbled the label

--- app/Filament/Resources/ProductResource/Pages/ProductVariationTypes.php
+++ app/Filament/Resources/ProductResource/Pages/ProductVariationTypes.php
@@ -17,12 +17,13 @@ class ProductVariationTypes extends EditRecord
 {
     protected static string $resource = ProductResource::class;
     protected static ?string $navigationIcon = 'heroicon-m-numbered-list';
+    protected static ?string $title = 'Variation Types';
 
     public function form(Form $form): Form
     {
         return $form->schema(
             [
-                Repeater::make('variationTypes')->relationship()->collapsible()->defaultItems(1)->addActionLabel('Add new variation type')->columns(2)->columnSpan(2)->schema([
+                Repeater::make('variationTypes')->label(false)->relationship()->collapsible()->defaultItems(1)->addActionLabel('Add new variation type')->columns(2)->columnSpan(2)->schema([
                     TextInput::make('name')->required(),
                     Select::make('type')->options(ProductVariationTypesEnum::lables())->required(),
                     Repeater::make('options')->relationship()->collapsible()->schema([
