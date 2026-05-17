7b1e00296307f1aded02b7b5d1e29103434d7c19
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-05-17 20:08:38 +0100

    change the cartitems into the minicartitems

--- resources/js/Components/App/MiniCardDropdown.tsx
+++ resources/js/Components/App/MiniCardDropdown.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import CurrencyFormatter from '../Core/CurrencyFormatter';
 
 function MiniCardDropdown() {
-    const {totalQuantity,totalPrice,cartItems} = usePage().props
+    const {totalQuantity,totalPrice,miniCartItems} = usePage().props
   return (
     <div className="dropdown dropdown-end">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
@@ -31,12 +31,12 @@ function MiniCardDropdown() {
         <div className="card-body">
           <span className="text-lg font-bold">{totalQuantity} Items</span>
           <div className="my-4 max-h-[300px] overflow-auto">
-            {cartItems.length === 0 && (
+            {miniCartItems.length === 0 && (
                 <div className="py-2 text-gray-500 text-center">
                     You don't have any items yet
                 </div>
             )}
-            {cartItems.map((item)=>(
+            {miniCartItems.map((item)=>(
                 <div key={item.id} className="flex gap-4 p-3">
                     <Link href={route('product.show', item.slug)} className='w-16 h-16 justify-center items-center'>
                         <img src={item.image} alt={item.title} className='max-w-full max-h-full' />
