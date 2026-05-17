4e7227a4819f322fe89ee22242d4ec0bcf160bc5
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-05-17 19:35:58 +0100

    Add the csrf token

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -40,6 +40,7 @@ class HandleInertiaRequests extends Middleware
 
         return [
             ...parent::share($request),
+            'csrf_token'=>csrf_token(),
             'auth' => [
                 'user' => $request->user(),
             ],
