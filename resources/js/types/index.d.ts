780770fda6320e37f04e11929b867617c71b6cba
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-05-17 11:51:14 +0100

    Change the lower case of the first letter into upper case of: select,radio,image

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -24,7 +24,7 @@ export type VariationTypeOption = {
 export type VariationType = {
     id: number;
     name: string;
-    type: 'select' | 'radio' | 'image';
+    type: 'Select' | 'Radio' | 'Image';
     options: VariationTypeOption[];
 }
 
