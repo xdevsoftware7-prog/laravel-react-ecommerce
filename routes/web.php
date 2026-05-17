3740dcdef8a9bf32c06ad039fe8dff7e565b5ed6
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-05-17 11:46:27 +0100

    Create a poste route for the cart.store

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,13 @@ Route::get('/products/{product:slug}', [ProductController::class, 'show'])->name
 //     return Inertia::render('Dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::post('/cart/store/{product}',function(){
+
+})->name('cart.store');
+
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
