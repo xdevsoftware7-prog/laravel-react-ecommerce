34778bd7d1f4be7de17dfc79d41d4b8822892d74
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-05-17 20:11:53 +0100

    Move the cart index

---
+++ resources/js/Pages/Cart/Index.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { GroupedCartItems, PageProps } from '@/types'
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
+import { Head, Link } from '@inertiajs/react'
+import CartItem from '@/Components/App/CartItem'
+import CurrencyFormatter from '@/Components/Core/CurrencyFormatter'
+import PrimaryButton from '@/Components/Core/PrimaryButton'
+import { CreditCardIcon } from '@heroicons/react/24/outline'
+function Index({
+    csrf_token, cartItems, totalQuantity, totalPrice
+}:PageProps<{cartItems: Record<number,GroupedCartItems>}>) {
+  return (
+    <AuthenticatedLayout>
+        <Head title='Your Cart'>
+            <div className="container mx-auto p-8 flex flex-col lg:flex-row gap-4">
+                <div className="card flex-1 bg-white dark:bg-gray-800 order-2 lg:order-1">
+                    <div className="card-body">
+                        <h2 className="text-lg font-bold">
+                            Shopping Cart
+                        </h2>
+                        <div className="my-4">
+                            {Object.keys(cartItems).length === 0 && (
+                                <div className="py-2 text-gray-500 text-center">
+                                    You don't have any items yet.
+                                </div>
+                            )}
+                            {Object.values(cartItems).map((cartItem)=>(
+                                <div key={cartItem.user.id} className="">
+                                    <div className="flex items-center justify-between pb-4 border-b border-gray-300 mb-4">
+                                        <Link href="/" className={"underline"}>{cartItem.user.name}</Link>
+                                        <div className="">
+                                            <form action={route('cart.checkout')} method='post'>
+                                                <input type="hidden" name='_token' value={csrf_token} />
+
+                                                <input type="hidden" name='vendor_id' value={cartItem.user.id} />
+                                                <button className='btn btn-sm btn-ghost'>
+                                                    <CreditCardIcon className="size-6">
+                                                        Pay Only for this seller
+                                                    </CreditCardIcon>
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                    {cartItem.items.map(item=>(
+                                        <CartItem item={item} key={item.id} ></CartItem>
+                                    ))}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <div className="card flex-1 bg-white dark:bg-gray-800 lg:min-w-[260px] order-1 lg:order-2">
+                    <div className="card-body">
+                            SubTotal ({totalQuantity} items): &nbsp;
+                            <CurrencyFormatter amount={totalPrice}></CurrencyFormatter>
+                            <form action={route('cart.checkout')} method='post'>
+                                <input type="hidden" name='_token' value={csrf_token} />
+                                <PrimaryButton className="rounded-full">
+                                    <CreditCardIcon className="size-6">Proceed to checkout</CreditCardIcon>
+                                </PrimaryButton>
+                            </form>
+                    </div>
+                </div>
+                
+            </div>
+        </Head>
+    </AuthenticatedLayout>
+  )
+}
+
+export default Index
