f18c00b1b6e64567d8aaee67dce498c984db41a4
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-05-17 18:17:46 +0100

    fix the bug of insertion a table

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class CartItem extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'quantity',
+        'price',
+        'variation_type_options_ids'
+    ];
+
+    // Indique à Laravel de gérer la conversion Array <-> JSON automatiquement
+    protected $casts = [
+        'variation_type_options_ids' => 'array', // ou 'json'
+    ];
 }
