4f30f3452ad4a83b0943e6e45048b54d316fc1c5
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-05-17 13:23:18 +0100

    Change the scope of published into the website

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
     public function home()
     {
         $products = Product::query()
-            ->published()
+            ->forWebsite()
             ->paginate(12);
 
         return Inertia::render('Home', [
