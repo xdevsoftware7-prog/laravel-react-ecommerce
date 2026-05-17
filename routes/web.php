7eb5a5ce33f4f36d199757e7115febdca0d401b8
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-05-17 20:14:43 +0100

    add the checkout cart route

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::middleware(['verified'])->group(function(){
+        Route::post('/cart/checkout',[CartController::class,'checkout'])->name('cart.checkout');
+    });
 });
 
 require __DIR__ . '/auth.php';
