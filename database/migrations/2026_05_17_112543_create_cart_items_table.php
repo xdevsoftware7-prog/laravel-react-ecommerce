1156b6237ca38035b8988128863e05c0145a9144
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-05-17 13:09:51 +0100

    Build the migration for the cart_items

--- database/migrations/2026_05_17_112543_create_cart_items_table.php
+++ database/migrations/2026_05_17_112543_create_cart_items_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('product_id')->index()->constrained('products')->cascadeOnDelete();
+            $table->foreignId('user_id')->index()->constrained('users');
+            $table->integer('quantity');
+            $table->decimal('price',20,4);
+            $table->json('variation_type_options_ids')->nullable();
+            $table->boolean('saved_for_later')->default(false);
             $table->timestamps();
         });
     }
