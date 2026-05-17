27d80eb818dc363cff959405b520ea1ade105083
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-05-17 14:50:16 +0100

    Finish implementing the getCartItem

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -3,7 +3,9 @@
 namespace App\Services;
 
 use App\Models\Product;
+use App\Models\VariationTypeOption;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class CartService
 {
@@ -25,13 +27,56 @@ class CartService
                 } else {
                     $cartItems = $this->getCarItemsFromCookies();
                 }
-                $productIds = collect($cartItems)->map(fn($item)=>$item['product_id']);
-                $products = Product::whereIn('id',$productIds)->with("user.vendor");
+                $productIds = collect($cartItems)->map(fn($item) => $item['product_id']);
+                $products = Product::whereIn('id', $productIds)->with("user.vendor")->forWebsite()->get()->keyBy("id");
+                $cartItemsData = [];
+                foreach ($cartItems as $key => $cartItem) {
+                    $product = data_get($products, $cartItem['product_id']);
+                    if (!$product) continue;
+
+                    $optionInfo = [];
+                    $options = VariationTypeOption::with("variationType")->whereIn('id', $cartItem['option_ids'])->get();
+
+                    $imageUrl = null;
+                    foreach ($cartItem['option_ids'] as $option_id) {
+                        $option = data_get($options, $option_id);
+                        if (!$imageUrl) {
+                            $imageUrl = $option->getFirstMediaUrl('images', 'small');
+                        }
+
+                        $optionInfo[] = [
+                            'id' => $option_id,
+                            'name' => $option->name,
+                            'type' => [
+                                'id' => $option->variationType->id,
+                                'name' => $option->variationType->name
+                            ]
+                        ];
+                    }
+                    $cartItemData[] = [
+                        'id'=>$cartItem['id'],
+                        'product_id'=>$product->id,
+                        'title'=>$product->title,
+                        'slug'=>$product->slug,
+                        'price'=>$cartItem['price'],
+                        'quantity'=>$cartItem['quantity'],
+                        'option_ids'=>$cartItem['option_ids'],
+                        'options'=>$optionInfo,
+                        'image'=>$imageUrl ?: $product->getFirstMediaUrl('images','small'),
+                        'user'=>[
+                            'id'=>$product->created_by,   
+                            'name'=>$product->user->vendor->store_name,
+                        ]
+                    ];
+                }
+
+                $this->cachedCartItems = $cartItemData;
             }
             return $this->cachedCartItems;
-        } catch (\Throwable $th) {
-            //throw $th;
+        } catch (\Exception $e) {
+            Log::error($e->getMessage(). PHP_EOL . $e->getTraceAsString());
         }
+        return [];
     }
     public function  getTotalQuantity() {}
     public function  getTotalPrice() {}
