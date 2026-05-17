cc0753baddb006340400dc15215ff2532e661d30
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-05-17 15:39:03 +0100

    implement getPriceForOptions funct

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -62,5 +62,18 @@ class Product extends Model implements HasMedia
 
    }
 
+   public function getPriceForOptions($optionIds = []){
+         $optionIds = array_values($optionIds);
+         sort($optionIds);
+         foreach($this->variations() as $variation){
+            $a = $variation->variation_type_option_ids;
+            sort($a);
+            if($optionIds == $a){
+                return $variation->price !== null ? $variation->price : $this->price;
+            }
+         }
+
+         return $this->price;
+   }
 
 }
