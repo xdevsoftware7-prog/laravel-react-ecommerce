ddb816e7bd715351ae8826562fca5be8a03e5894
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-05-17 13:12:36 +0100

    Start implementing the CartService getCartItems mth

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -3,14 +3,49 @@
 namespace App\Services;
 
 use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 
 class CartService
 {
-    public function addItemToCart(Product $product, int $quantity=1, $optionIds = null){
+    private ?array $cachedCartItems = null;
+    protected const COOKIE_NAME = 'cartItems';
+    protected const COOKIE_LIFETIME = 60 * 24 * 365; // 1 year
 
+
+    public function addItemToCart(Product $product, int $quantity = 1, $optionIds = null) {}
+
+    public function updateItemQuantity(int $productId, int $quantity, $optionIds = null) {}
+    public function removeItemFromCart(int $productId, $optionIds = null) {}
+    public function  getCartItems()
+    {
+        try {
+            if ($this->cachedCartItems === null) {
+                if (Auth::check()) {
+                    $cartItems = $this->getCarItemsFromDatabase();
+                } else {
+                    $cartItems = $this->getCarItemsFromCookies();
+                }
+                $productIds = collect($cartItems)->map(fn($item)=>$item['product_id']);
+                $products = Product::whereIn('id',$productIds)->with("user.vendor");
+            }
+            return $this->cachedCartItems;
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
     }
+    public function  getTotalQuantity() {}
+    public function  getTotalPrice() {}
 
-    public function updateItemQuantity(int $productId, int $quantity, $optionIds = null){
+    protected function updateItemQuantityInDatabase(int $productId, int $quantity, array $optionIds): void {}
 
-    }
+    protected function saveItemToDatabase(int $productId, int $quantity, array $optionIds): void {}
+
+    protected function saveItemToCookies(int $productId, int $quantity, array $optionIds): void {}
+
+    protected function removeItemFromDatabase(int $productId, int $quantity, array $optionIds): void {}
+
+    protected function removeItemFromCookies(int $productId, int $quantity, array $optionIds): void {}
+
+    protected function getCarItemsFromDatabase() {}
+    protected function getCarItemsFromCookies() {}
 }
